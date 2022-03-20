68802a8b451206cc4bf35a62a9057180420363ce
rayjosong <rayjosong@gmail.com>
2022-03-21 00:26:00 +0800

    try oauth api

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
 var express = require("express");
 var app = express();
 app.listen(8080, () => {
- console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
 
 app.get("/", (req, res) => {
- res.send(["Hello! This is version 0.18"]);
+  res.send(["Hello! This is version 0.18"]);
 });
 
 app.get("/api", (req, res, next) => {
- res.json(["Lorem","Ipsum","Dolor","Sit","Amet"]);
+  res.json(["Lorem", "Ipsum", "Dolor", "Sit", "Amet"]);
 });
 
 app.get("/fx-static", (req, res, next) => {
- res.json({"sell": "589.181", "timestamp": "2020-06-17 14:42:34.492974", "buy": "489.1"});
+  res.json({
+    sell: "589.181",
+    timestamp: "2020-06-17 14:42:34.492974",
+    buy: "489.1",
+  });
 });
 
 app.get("/fx", (req, res, next) => {
- rand1 = Math.floor(Math.random() * 90) + 10;
- rand2 = Math.floor(Math.random() * 90) + 10;
+  rand1 = Math.floor(Math.random() * 90) + 10;
+  rand2 = Math.floor(Math.random() * 90) + 10;
 
- var today = new Date();
- var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
- var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
- var dateTime = date+' '+time;
+  var today = new Date();
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + " " + time;
 
- res.json({"sell": "1587." + rand1, "timestamp": dateTime, "buy": "1487." + rand2});
+  res.json({
+    sell: "1587." + rand1,
+    timestamp: dateTime,
+    buy: "1487." + rand2,
+  });
 });
 
 app.get("/random", (req, res, next) => {
- res.json([Math.floor(Math.random() * 90) + 10]);
+  res.json([Math.floor(Math.random() * 90) + 10]);
 });
 
-app.get('*',function (req, res) {
-        res.redirect('/');
-    });
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
 
 // node app.js
+// test
+//test
